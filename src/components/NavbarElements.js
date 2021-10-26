4bd69fc96543c9c4f7f2cf1c9b64d2cba8549421
cianjameskelly <59668336+cianjameskelly@users.noreply.github.com>
2021-10-26 17:27:50 -0400

    added model component

--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import * as ReactBootStrap from 'react-bootstrap'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-//import Dashboard from '../Dashboard'
+import Dashboard from './Dashboard'
 import About from './About';
 import Home from './Home';
 import Polis from './Polis';
+import { FaHome } from 'react-icons/fa'
+import { MdPerson } from 'react-icons/md'
+import { HiUserGroup } from 'react-icons/hi'
+import { BsFillQuestionSquareFill } from 'react-icons/bs'
 
 export default function NavbarElements() {
     return (
-        <Router minwidth={100}>
+        <Router>
         <div>
             <ReactBootStrap.Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
                 <ReactBootStrap.Container>
-                    <ReactBootStrap.Navbar.Brand href="#home">VoteSource</ReactBootStrap.Navbar.Brand>
+                    <ReactBootStrap.Navbar.Brand>VoteSource</ReactBootStrap.Navbar.Brand>
                     <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                         <ReactBootStrap.Nav className="me-auto">
-                            <ReactBootStrap.Nav.Link as={Link} to={'/home'}>Home</ReactBootStrap.Nav.Link>
-                            <ReactBootStrap.Nav.Link as={Link} to={'/polis'}>Candidates</ReactBootStrap.Nav.Link>
-                            <ReactBootStrap.Nav.Link as={Link} to={'/dashboard'}>Profile</ReactBootStrap.Nav.Link>
-                            <ReactBootStrap.Nav.Link as={Link} to={'/about'}>About</ReactBootStrap.Nav.Link>
+                            <ReactBootStrap.Nav.Link as={Link} to={'/home'}><FaHome></FaHome> Home</ReactBootStrap.Nav.Link>
+                            <ReactBootStrap.Nav.Link as={Link} to={'/polis'}><HiUserGroup></HiUserGroup> Candidates</ReactBootStrap.Nav.Link>
+                            <ReactBootStrap.Nav.Link as={Link} to={'/dashboard'}><MdPerson></MdPerson> Profile</ReactBootStrap.Nav.Link>
+                            <ReactBootStrap.Nav.Link as={Link} to={'/about'}><BsFillQuestionSquareFill></BsFillQuestionSquareFill> About</ReactBootStrap.Nav.Link>
                         </ReactBootStrap.Nav>
                     </ReactBootStrap.Navbar.Collapse>
                 </ReactBootStrap.Container>
--- src/components/Polis.js
+++ src/components/Polis.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Modal, Row } from 'react-bootstrap';
 import app from '../firebase'
-//import { SearchState, IntegratedFiltering } from '@devexpress/dx-react-grid';
 import { BsChevronRight } from 'react-icons/bs'
-import moreInfo from './moreInfo'
+//import { FaRepublican, FaDemocrat } from 'react-icons/fa'
 
 function Polis() {
     const [politicians, setPolis] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    const ref = app.firestore().collection("politicians");
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
+    const ref = app.firestore().collection("politicians");
+   
     function getPolis() {
         setLoading(true);
         ref.onSnapshot((querySnapshot) => {
@@ -31,13 +34,18 @@ function Polis() {
         return <h1>Loading...</h1>;
     }
 
+/*
+    function party_icon() {
+        if (politician.party='Democrat') {
+          return <FaDemocrat />;
+        } else {
+        return <FaRepublican />;
+      }
+    }
+*/
     return (
         <div min-width= {100}>          
             <h1>Politicians</h1>
-            {/*
-            <SearchState defaultValue='Type a Politician name, party, position, etc...' />
-            <IntegratedFiltering />
-            */}
             <Row xs={2} md={5} lg={8}>
                 {politicians.map((politician) => (
                     <Col>
@@ -45,20 +53,35 @@ function Polis() {
                             style={{ width: '15rem' }}
                             key={politician.id}
                             >
-                            <Card.Img variant='top' src='https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE=' />
+                            <Card.Img src={politician.img} />
                             <Card.Body>
                                 <Card.Title>{politician.name}</Card.Title>
                                 <Card.Text>
                                     <h6>{politician.position}</h6>
                                     <h6>{politician.party}</h6>
+                                    {/*<h6>{this.party_icon(politician.party)}</h6>*/}
+                                    <Button
+                                        variant='primary'
+                                        onClick={handleShow}
+                                    >
+                                        <BsChevronRight></BsChevronRight>
+                                    </Button>
+                                    <Modal show={show} onHide={handleClose}>
+                                        <Modal.Header>
+                                            <Modal.Title>{politician.name}</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <h6>Position: {politician.position}</h6>
+                                            <h6>Party: {politician.party}</h6>
+                                            <p>Webscraping for politician information is supposed to occur here</p>
+                                        </Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleClose}>
+                                                Close
+                                            </Button>
+                                        </Modal.Footer>
+                                    </Modal>
                                 </Card.Text>
-                                <Button 
-                                >
-                                    <BsChevronRight></BsChevronRight>
-                                </Button>
-                                <moreInfo trigger={true}>
-                                    
-                                </moreInfo>
                             </Card.Body>
                         </Card>
                     </Col>
